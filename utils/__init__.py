4e6ba096ce237afe142d47c943217e0cee286afc
Mojtaba Moodi <mojtabam@klogin03.paice.vectorinstitute.ai>
2026-01-22 13:50:16 -0500

    Export STANDARD_CHANNEL_NAMES from utils package
    - Add STANDARD_CHANNEL_NAMES to utils package exports
    - Enables easy import: from utils import STANDARD_CHANNEL_NAMES

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,6 +3,7 @@ Utilities package for EEG Classification Framework
 """
 
 from .json_utils import safe_json_dump, safe_json_load, convert_numpy_types, NumpyEncoder
+from .eeg_constants import STANDARD_CHANNEL_NAMES
 
-__all__ = ['safe_json_dump', 'safe_json_load', 'convert_numpy_types', 'NumpyEncoder']
+__all__ = ['safe_json_dump', 'safe_json_load', 'convert_numpy_types', 'NumpyEncoder', 'STANDARD_CHANNEL_NAMES']
 
